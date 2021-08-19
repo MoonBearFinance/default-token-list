b64c0ec21acd747efaf8ac66c2a510fb157ade59
Peezebit <peezenut@gmail.com>
2021-08-19 08:18:15 -0500

    add BNB only

--- src/buildList.js
+++ src/buildList.js
@@ -1,9 +1,9 @@
 const { version } = require("../package.json");
-const mainnet = require("./tokens/mainnet.json");
-const ropsten = require("./tokens/ropsten.json");
-const rinkeby = require("./tokens/rinkeby.json");
-const goerli = require("./tokens/goerli.json");
-const kovan = require("./tokens/kovan.json");
+// const mainnet = require("./tokens/mainnet.json");
+// const ropsten = require("./tokens/ropsten.json");
+// const rinkeby = require("./tokens/rinkeby.json");
+// const goerli = require("./tokens/goerli.json");
+// const kovan = require("./tokens/kovan.json");
 const bsc = require("./tokens/bsc.json");
 
 module.exports = function buildList() {
@@ -19,9 +19,10 @@ module.exports = function buildList() {
     tags: {},
     logoURI: "ipfs://QmNa8mQkrNKp1WEEeGjFezDmDeodkWRevGFN8JCV7b4Xir",
     keywords: ["uniswap", "default"],
-    tokens: [...mainnet, ...ropsten, ...goerli, ...kovan, ...rinkeby, ...bsc]
+    //[...mainnet, ...ropsten, ...goerli, ...kovan, ...rinkeby, ...bsc]
+    tokens:
       // sort them by symbol for easy readability
-      .sort((t1, t2) => {
+      [...bsc].sort((t1, t2) => {
         if (t1.chainId === t2.chainId) {
           return t1.symbol.toLowerCase() < t2.symbol.toLowerCase() ? -1 : 1;
         }
